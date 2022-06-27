77c41bd84a524d8bffa35759f4c9e5e0ddbd4f7f
saiful110816 <saifulrmail@gmail.com>
2022-06-28 01:11:10 +0600

    Added a Procfile.

---
+++ index.js
@@ -0,0 +1,116 @@
+let express = require('express');
+require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const { get } = require('mongoose');
+var ObjectId = require('mongodb').ObjectID;
+
+let app = express();
+const uri =process.env.MONGODB;
+
+app.get('/', async (req, res)=>{
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      await client.connect();
+      // database and collection code goes here
+      const db = client.db("App");
+      const coll = db.collection("Faild_status");
+      // find code goes here
+      const cursor = coll.find();
+      // iterate code goes here
+      let arr  = [], count=0;
+      await cursor.forEach(function(x){
+
+        arr.push(x)
+        count++;
+      })
+      arr.push({total: count})
+    //  await console.log(cursor)
+    res.json(arr)
+
+
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+})
+
+app.get('/add', async (req, res)=>{
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      await client.connect();
+      // database and collection code goes here
+      const db = client.db("App");
+      const coll = db.collection("Faild_status");
+      // find code goes here
+    let p = await  coll.insertOne({time: new Date(new Date().getTime()-3*60*60*1000), type:0})
+     res.json(p)
+
+
+
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+})
+
+app.get('/dellast', async (req, res)=>{
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      await client.connect();
+      // database and collection code goes here
+      const db = client.db("App");
+      const coll = db.collection("Faild_status");
+      // find code goes here
+     p = await coll.deleteOne({_id:ObjectId("62b84599fe589c90bdb234da")})
+    res.send(p)
+    
+
+
+
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+})
+
+app.get('/update', async (req, res)=>{
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      await client.connect();
+      // database and collection code goes here
+      const db = client.db("App");
+      const coll = db.collection("Faild_status");
+      // find code goes here
+    await  coll.findOneAndUpdate({"_id": ObjectId("57a9aa24e93864e02d91283c")},
+    {$set:{type:"1"}},
+    {upsert:true})
+    res.send('done')
+    
+
+
+
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+})
+
+app.listen(3000, ()=>{
+    console.log('app listing on port 3000')
+})
+
+
+// Replace the uri string with your MongoDB deployment's connection string.
+
